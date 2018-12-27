d354d14b77e4f5a1e2bed0632ecc648a97982b5f
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2018-12-27 10:58:10 -0800

    Started with a little JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// Create an array of Words
+const words = ['MF DOOM', 'Andre 3000', 'Ghostface Killa', 'Method Man','Lauryn Hill', 'King Los', 'Mos Def', 'Eminem','Redman', 'Nas']
+// Choose word randomly
+// Create underscores based on lenght of word
+// Get Users guesses
+// Check if guess is right
+// if right push to right array
+// if incorrect push to wrong array
