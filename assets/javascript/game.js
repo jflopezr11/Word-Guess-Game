e2bfd670dc2b3c1870d67b5d2390b3c23c39f9ed
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2018-12-28 13:02:01 -0800

    Some more JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,25 @@
 // Create an array of Words
-const words = ['MF DOOM', 'Andre 3000', 'Ghostface Killa', 'Method Man','Lauryn Hill', 'King Los', 'Mos Def', 'Eminem','Redman', 'Nas']
+const word = ['MFDOOM', 'Andre3000', 'GhostfaceKilla', 'MethodMan','LaurynHill', 'KingLos', 'MosDef', 'Eminem','Redman', 'Nas']
 // Choose word randomly
+let randNum = Math.floor(Math.random() * word.length);
+let choosenWord = word[randNum]
+let underScore = [];
+console.log(choosenWord);
 // Create underscores based on lenght of word
+let generateUnderscore = () => {
+    for(let i = 0; i < choosenWord.length; i++) {
+        underScore.push('_');
+    }
+    return underScore;
+}
+
+console.log(generateUnderscore());
 // Get Users guesses
+document.addEventListener('keypress', (event) => {   
+    let keyword = String.fromCharCode (event.keyCode);
+    console.log(keyword);
+
+});
 // Check if guess is right
 // if right push to right array
 // if incorrect push to wrong array
